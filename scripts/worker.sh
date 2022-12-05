a0394d32686a7700d22c66bf55e489ad9d97a4fa
heziiop <1624120705@qq.com>
2022-12-05 16:03:48 +0800

    worker等待master完成后再加入

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+until [ -f /vagrant/configs/join.sh ]
+do
+    sleep 1
+done
+
 /bin/bash /vagrant/configs/join.sh -v
 
-sudo -i -u vagrant bash << EOF
-mkdir -p /home/vagrant/.kube
-sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
-sudo chown 1000:1000 /home/vagrant/.kube/config
-EOF
+# sudo -i -u vagrant bash << EOF
+# mkdir -p /home/vagrant/.kube
+# sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
+# sudo chown 1000:1000 /home/vagrant/.kube/config
+# EOF
